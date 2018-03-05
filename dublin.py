f52c2f64cbef9891aae480d00ba948a47106da7e
Piotr Masztalski <masztalski.piotr@gmail.com>
2018-03-05 20:45:20 +0100

    Added class methods to calculate time and distance

--- dublin.py
+++ dublin.py
@@ -5,8 +5,14 @@ class Car:
     def __init__(self):
         self.row = 0
         self.column = 0
+        self.is_driving = False
+        self.driving_steps_left = 0
+        self.ride_idxs = []
 
-    def change_pos(self, x, y):
+    def get_loc(self):
+        return self.row, self.column
+
+    def change_loc(self, x, y):
         self.row = x
         self.column = y
 
@@ -21,19 +27,36 @@ class Ride:
         self.earliest_start = earliest_start
         self.latest_finish = latest_finish
 
+    def get_start_loc(self):
+        return self.start_row, self.start_column
 
-with open('b_should_be_easy.in') as easy:
+    def get_end_loc(self):
+        return self.end_row, self.end_column
+
+    def ride_duration(self):
+        return count_distance(self.get_start_loc(), self.get_end_loc())
+
+    def max_ride_duration(self):
+        return self.latest_finish - self.earliest_start
+
+
+# function that returns the distance between two intersections
+def count_distance(a, b):
+    return sum(tuple(abs(x - y) for x, y in zip(a, b)))
+
+
+# extracting data from the input file
+with open('a_example.in', 'r') as input_file:
     data_lines = []
-    for line in easy:
+    for line in input_file:
         data_lines.append(line.split())
 
-
-rows = int(data_lines[0][0])
-columns = int(data_lines[0][1])
-vehicles = int(data_lines[0][2])
-rides = int(data_lines[0][3])
+rows_count = int(data_lines[0][0])
+columns_count = int(data_lines[0][1])
+vehicles_count = int(data_lines[0][2])
+rides_count = int(data_lines[0][3])
 bonus = int(data_lines[0][4])
-steps = int(data_lines[0][5])
+steps_count = int(data_lines[0][5])
 
 start_rows = []
 start_columns = []
@@ -50,18 +73,23 @@ for line in data_lines[1:]:
     earliest_starts.append(int(line[4]))
     latest_finishes.append(int(line[5]))
 
+# initializing rides array
+rides = []
 
-rides_array = []
+for i in range(rides_count):
+    rides.append(Ride(start_rows[i], start_columns[i],
+                      end_rows[i], end_columns[i],
+                      earliest_starts[i], latest_finishes[i]))
 
-for i in range(rides):
-    rides_array.append(Ride(start_rows[i], start_columns[i],
-                            end_rows[i], end_columns[i],
-                            earliest_starts[i], latest_finishes[i]))
-
+# initializing cars array
 cars = []
 
-for i in range(vehicles):
+for i in range(vehicles_count):
     cars.append(Car())
 
-
-print(rides_array[0].latest_finish)
+# simulation loop
+with open('out.txt', 'a') as output_file:
+    for i in range(0, steps_count):
+        for j in range(0, vehicles_count):
+            for k in range(0, rides_count):
+                pass
